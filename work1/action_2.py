feae2c8d208c0165a07e1bb46bdbb66409afa120
Pengyu1224 <66915872+Pengyu1224@users.noreply.github.com>
2020-06-15 00:47:26 +0800

    Add files via upload

---
+++ work1/action_2.py
@@ -0,0 +1,13 @@
+import numpy as np
+persontype=np.dtype({'names':['name','Chinese','Math','English'],'formats':['S32','i','i','i']})
+peoples=np.array([('Zhangfei',68,65,30),('Guanyu',95,76,98),('Liubei',98,86,88),('Dianwei',90,88,77),('Xuchu',80,90,90)],dtype=persontype)
+ChineseScore=peoples[:]['Chinese']
+MathScore=peoples[:]['Math']
+EnglishScore=peoples[:]['English']
+print('语文、数学、英语平均成绩分别为：',np.mean(ChineseScore),np.mean(MathScore),np.mean(EnglishScore))
+print('语文、数学、英语最好成绩分别为：',np.max(ChineseScore),np.max(MathScore),np.max(EnglishScore))
+print('语文、数学、英语最差成绩分别为：',np.min(ChineseScore),np.min(MathScore),np.min(EnglishScore))
+print('语文、数学、英语成绩方差分别为：',np.var(ChineseScore),np.var(MathScore),np.var(EnglishScore))
+print('语文、数学、英语成绩标准差分别为：',np.std(ChineseScore),np.std(MathScore),np.std(EnglishScore))
+ranking=sorted(peoples,key=lambda x:x[1]+x[2]+x[3],reverse=True)
+print('成绩排名为：',ranking)
